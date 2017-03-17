ded6e9218d29f86e63f4dd2bb19a4ffa61a49dcc
admin <admin@192.168.56.1>
2017-03-17 21:17:36 +0900

    상원9시17분

--- Balpoom/src/controller/main_controller.java
+++ Balpoom/src/controller/main_controller.java
@@ -1,11 +1,21 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import models.roomDao;
+
 @Controller
 public class main_controller {
+	
+	@Autowired
+	roomDao rd;
+	
 	@RequestMapping("/")
 	public ModelAndView mainHandler() {
 		ModelAndView mav = new ModelAndView("t_main");
@@ -21,6 +31,17 @@ public class main_controller {
 	@RequestMapping("/detail")
 	public ModelAndView detailViewHandler() {
 		ModelAndView mav = new ModelAndView("t_detail");
+		return mav;
+	}
+	//test 용 신경쓰지 말것
+	@RequestMapping("/testing")
+	public ModelAndView testing(){
+		ModelAndView mav = new ModelAndView();
+		List<HashMap<String, Object>> mlist = rd.test_room();
+	
+		mav.addObject("mlist",mlist);
+		mav.addObject("mlistsize",mlist.size());
+		mav.setViewName("/testing/test");
 		return mav;
 	}
 
