851aef35feaf7c3a996bc22552ad9d0e6f5bf677
jakeeum <codeback9919@gmail.com>
2017-03-20 21:09:18 +0900

    제발

--- Balpoom/WebContent/javascript/page.js
+++ Balpoom/WebContent/javascript/page.js
@@ -1,129 +1,320 @@
 
+//<div style="min-height: 125px; background-color: white; margin: 2%">
+//<table>
+//	<tr>
+//		<td rowspan="3"><img src="/images/room.jpg"
+//			style="height: 120px; width: 120px;"></td>
+//		<td style="padding-left: 10px; vertical-align: bottom;"><span
+//			style="padding: 3px; background-color: #04B486; border-radius: 5px; color: white;">월세</span>
+//			<b
+//			style="color: #29B172; font-size: 20px; vertical-align: middle;">
+//				2000/30 </b></td>
+//	</tr>
+//	<tr>
+//		<td style="padding-left: 10px; font-size: 14px;">
+//										귀한 반전세 풀옵션 원룸입니다
+//
+//
+//
+//		</td>
+//	</tr>
+//	<tr>
+//		<td
+//			style="padding-left: 10px; font-size: 12px; color: gray; vertical-align: text-top;">
+//			원룸 | 3층 | 21.4m<sup>2</sup> | 관리비 5만원
+//		</td>
+//	</tr>
+//</table>
+//</div>
+//<hr style="margin: 5px;" />
+//
+//<div style="min-height: 125px; background-color: white; margin: 2%">
+//<table>
+//	<tr>
+//		<td rowspan="3"><img src="/images/room2.jpg"
+//			style="height: 120px; width: 120px;"></td>
+//		<td style="padding-left: 10px; vertical-align: bottom;"><span
+//			style="padding: 3px; background-color: #04B486; border-radius: 5px; color: white;">월세</span>
+//			<b
+//			style="color: #29B172; font-size: 20px; vertical-align: middle;">
+//				2000/30 </b></td>
+//	</tr>
+//	<tr>
+//		<td style="padding-left: 10px; font-size: 14px;">귀한 반전세 풀옵션
+//			원룸입니다</td>
+//	</tr>
+//	<tr>
+//		<td
+//			style="padding-left: 10px; font-size: 12px; color: gray; vertical-align: text-top;">
+//			원룸 | 3층 | 21.4m<sup>2</sup> | 관리비 5만원
+//		</td>
+//
+//	</tr>
+//</table>
+//
+//</div>
 
-	function initMap() {
-		//var pos = { lat : ${json.results[0].geometry.location.lat },lng : ${json.results[0].geometry.location.lng } };
-		var map = new google.maps.Map(document.getElementById('map'), {
-			"center" : { lat: 37.5172363, lng: 127.0473248},											// 변수명으로 설정 가능		
-			"scrollwheel" : true,	//false 로 할경우 마우스 휠 작동이 안된다
-			"zoom" : 15
-		});
-		 var marker = new google.maps.Marker({
-	          "map" : map,	// 이 map 은 위에 var map 
-	          "position" : { lat: 37.5172363, lng: 127.0473248}, 									// 변수명으로 설정 가능	
-	          "title" : '${param.addr}'
-	    });
+function initMap() {
+	// var pos = { lat : ${json.results[0].geometry.location.lat },lng :
+	// ${json.results[0].geometry.location.lng } };
+	var map = new google.maps.Map(document.getElementById('map'), {
+		"center" : {
+			lat : 37.5172363,
+			lng : 127.0473248
+		}, // 변수명으로 설정 가능
+		"scrollwheel" : true, // false 로 할경우 마우스 휠 작동이 안된다
+		"zoom" : 15
+	});
+	var marker = new google.maps.Marker({
+		"map" : map, // 이 map 은 위에 var map
+		"position" : {
+			lat : 37.5172363,
+			lng : 127.0473248
+		}, // 변수명으로 설정 가능
+		"title" : '${param.addr}'
+	});
+}
+
+setTimeout(initMap, 1000);
+var arrl = "empty";
+var jpgs = "empty";
+var start = 0;
+var end = 3;
+var curr = 0;
+var njj = "empty";
+var dn = 7750005;
+
+
+
+
+var PagingHelper = {
+
+	'data' : {
+		currentPage : 1 // 현재페이지
+		// ,startPage : 1 // 시작페이지
+		,
+		pageSize : 50 // 페이지 사이즈 (화면 출력 페이지 수)
+		,
+		maxListCount : 5 // (보여질)최대 리스트 수 (한페이지 출력될 항목 갯수)
+		,
+		startnum : 1 // 시작 글번호
+		,
+		lastnum : 50 // 마지막 글번호
+		,
+		totalCnt : 0 // 전체 글의 갯수.
+		,
+		totalPageCnt : 0
+	// 전체 페이지 수
+	},
+	'setOption' : function(opt) {
+		if (typeof opt != 'object')
+			return;
+		for (key in opt) {
+			if (key in this.data) {
+				this.data[key] = opt[key]; // data에 입력받은 설정값 할당.
+			}
+		}
+	},
+	'nj' : function(arr){
+		console.log("njj addded")
+		njj = arr;
+	},
+	'dn' : function(dnn){
+		console.log("njj addded")
+		dn = dnn;
+	},
+
+	'arrlist' : function(arr) {
+		console.log("arrlist1");
+		arrl = arr;
+	},
+	'jpglist' : function(jpg){
+		console.log('jpg.list.entered');
+		jpgs = jpg;
+		//var obj = arrl[i];
+		//sb+= obj['B_RKIND'] 
+		for(var i = 0; i < 2; i++){
+			var obj = jpgs[i];
+			console.log(jpgs.length);
+			console.log(obj);
+		}
+	},
+	'linkajax' : function(num){
+		console.log("linkajax "+num  +" menu=" + njj[num]);
+		location.href='/detail?num='+njj[num];
+			
+	
+	},
+
+	'shHtml' : function(n_block) {
+		var _ = this;
+		if (typeof n_block == 'undefined')n_block = curr;
+		console.log("shHtml");
+
+		var sb = '';
+		var sbTemp = '';
+	
+		if (arrl == 'empty') {
+			curr = 1;
+		} else {
+			curr = n_block
+		}
+		end = (curr * 4) - 1;
+		start = end - 3;
+		console.log("total_count="+_.data.totalCnt + " end="+ end);
+		if(end >= _.data.totalCnt) {end = _.data.totalCnt -1; }
+
+		//console.log("curr=" + curr + " start=" + start + " end=" + end );
+		//console.log(arrl);
+
+		for (var i = start; i <= end; i++) {
+			var obj = arrl[i];
+			var obj2 = jpgs[i];
+			sb+="<div style='min-height: 125px; background-color: white; margin: 2%'>";
+			sb+="<table>"
+			sb+="<tr>";
+			sb+="<td rowspan='3'>";
+					//sb+="<img src="+'/images/room.jpg'+" style='height: 120px; width: 120px;'>";
+			sb+= "<li class='first' onclick='PagingHelper.linkajax("+ i +");'>";		
+			sb+="<img src="+obj2+" style='height: 120px; width: 120px;'>";
+			sb+= "</li>";
+			
+			
+			sb+="</td>";
+			sb+="<td style='padding-left: 10px; vertical-align: bottom;'>";
+			sb+="<span style='padding: 3px; background-color: #04B486;border-radius: 5px; color: white;'>" + obj['B_MPAY'] +"</span>";
+			sb+="<b style='color: #29B172; font-size: 20px; vertical-align: middle;'>"+ "2000/30" + "</b>";
+			sb+="</td>";
+			sb+="</tr>";
+			
+
+			sb+="<tr>";
+			
+				sb+="<td style='padding-left: 10px; font-size: 14px;'>";
+						sb+="귀한 반전세 풀옵션 원룹입니다<br/>";
+						sb+= obj['B_TITLE'];
+				sb+="</td>";
+			sb+="</tr>";
+			
+			
+			sb+="<tr>";
+				sb+="<td style='padding-left: 10px; font-size: 12px; color: gray; vertical-align: text-top;'>";
+				sb+= obj['B_RKIND'] + "|" + "3층  관리비 5만원";
+				sb+= "</td>";
+			sb+="</tr>";
+			sb+="</table>";
+			sb+="</div>";
+		
+			sb+="<hr style='margin: 30px;'/>";
+		}
+		// console.log(sb);
+		return sb;
+
+	},
+	
+
+	'button' : function(){
+		if($('#btt').data('clicked')){
+			console.log("if");
+		}else{
+			console.log("else");
+		}
+		
+		
+	},
+	
+
+	'pagingHtml' : function(pTotalCnt) {
+
+		console.log("pagingHtml" + pTotalCnt);
+
+		var _ = this;
+
+		_.data['totalCnt'] = pTotalCnt ? pTotalCnt : _.data['totalCnt'];
+
+		if (_.data['totalCnt'] == 0) {
+			return "";
+		}
+		// 총페이지수 구하기 : 페이지 출력 범위 (1|2|3|4|5)
+		_.data.totalPageCnt = Math.ceil(_.data.totalCnt / _.data.maxListCount);
+
+		// 현재 블럭 구하기
+		var n_block = Math.ceil(_.data.currentPage / _.data.pageSize);
+		this.shHtml(this.data.currentPage);
+		// 페이징의 시작페이지와 끝페이지 구하기
+		var s_page = (n_block - 1) * _.data.pageSize + 1; // 현재블럭의 시작 페이지
+		var e_page = n_block * _.data.pageSize; // 현재블럭의 끝 페이지
+		//console.log(s_page+"s_page"+ "e_page"+e_page);
+		var sb = '';
+		var sbTemp = '';
+
+		// 블럭의 페이지 목록 및 현재페이지 강조
+		for (var j = s_page; j <= e_page; j++) {
+			if (j > _.data.totalPageCnt)
+				break;
+			if (j == _.data.currentPage) {
+				sbTemp += "<li class='selected'>[<font color='red'>" + j + "</font>]</li>";
+			} else {
+				sbTemp += "<li onclick='PagingHelper.gotoPage(" + j + ");'>["
+						+ j + "]</li>";
+			}
+		}
+
+		// 이전페이지 버튼
+		sb = "<ul>"
+		if (_.data.currentPage > s_page
+				|| _.data.totalCnt > _.data.maxListCount && s_page > 1) {
+			sb += "<li class='first' onclick='PagingHelper.gotoPage(1);'>처음</li >"
+			sb += "<li class='previous' onclick='PagingHelper.gotoPage("
+					+ (_.data.currentPage - 1) + ");'>이전</li>"
+		}
+
+		// 현재블럭의 페이지 목록
+		sb += sbTemp
+
+		// 다음페이지 버튼
+		if (_.data.currentPage < _.data.totalPageCnt) {
+			sb += "<li class='next' onclick='PagingHelper.gotoPage("
+					+ (_.data.currentPage + 1) + ");'>다음</li>"
+			sb += "<li class='last' onclick='PagingHelper.gotoPage("
+					+ (_.data.totalPageCnt) + ");'>마지막</li >"
+		}
+		sb += "</ul>";
+
+		return sb;
+	},
+	"makeNum" : function(className, content) { // 필요없음.
+		return "<li class='" + className + "''>[" + content + "]</li>";
+	},
+	'setStartnumEndnum' : function() {
+		// 시작 글번호
+		this.data.startnum = (this.data.currentPage - 1)
+				* this.data.maxListCount + 1;
+
+		// 마지막 글번호
+		var tmp = this.data.currentPage * this.data.maxListCount;
+		this.data.lastnum = (tmp > this.data.totalCnt ? this.data.totalCnt
+				: tmp);
+	},
+	'gotoPage' : function(pageNum) {
+		console.log("gotoPage");
+
+		this.data.currentPage = pageNum; // 입력받은 페이지번호를 현재페이지로 설정
+		this.setStartnumEndnum(); // 입력받은 페이지의 startnum과 endnum구하기
+
+		// 콘솔 출력 (삭제)
+		console.log(this.data.currentPage + "/" + this.data.startnum + "/"
+				+ this.data.lastnum);
+		// alert(this.data.currentPage+"/"+this.data.startnum
+		// +"/"+this.data.lastnum);
+		// this.shHtml(this.data.currentPage);
+		// 리스트 불러오는 ajax 호출
+		// ////////////////////////
+
+		$("#paging").html(this.pagingHtml());
+		$("#sh").html(this.shHtml());
+
 	}
-	
-	setTimeout(initMap,1000);
+}
 
 
-    var PagingHelper = {
-        
-    	'data' : {
-             currentPage :1     // 현재페이지
-            //,startPage : 1    // 시작페이지
-            ,pageSize : 5       // 페이지 사이즈 (화면 출력 페이지 수)
-            ,maxListCount : 10  // (보여질)최대 리스트 수 (한페이지 출력될 항목 갯수)
-            ,startnum : 1       // 시작 글번호
-            ,lastnum : 50       // 마지막 글번호
-            ,totalCnt : 0       // 전체 글의 갯수.
-            ,totalPageCnt : 0   // 전체 페이지 수    
-        },
-        'setOption' : function(opt){
-            if( typeof opt != 'object' ) return;
-            for (key in opt ) {
-                if(key in this.data) {
-                    this.data[key] = opt[key]; //data에 입력받은 설정값 할당.
-                }
-            }
-        },
-        'pagingHtml' : function(pTotalCnt){
-            
-            var _ = this;
-            
-            _.data['totalCnt'] = pTotalCnt?pTotalCnt:_.data['totalCnt'];        
-            
-            if (_.data['totalCnt'] == 0) {
-                return "";
-            }
-            //총페이지수 구하기 : 페이지 출력 범위 (1|2|3|4|5)
-            _.data.totalPageCnt = Math.ceil(_.data.totalCnt / _.data.maxListCount);             
-            
-            //현재 블럭 구하기 
-            var n_block = Math.ceil(_.data.currentPage / _.data.pageSize); 
-                    
-            //페이징의 시작페이지와 끝페이지 구하기
-            var s_page = (n_block - 1) * _.data.pageSize + 1; // 현재블럭의 시작 페이지
-            var e_page = n_block * _.data.pageSize; // 현재블럭의 끝 페이지
-                
-            var sb='';
-            var sbTemp ='';
-            
-            //console.log(_.data);
-            //console.log(n_block+"/"+s_page+"/"+e_page);
-              
-            // 블럭의 페이지 목록 및 현재페이지 강조
-            for (var j = s_page; j <= e_page; j++) {                
-                if (j > _.data.totalPageCnt ) break;                         
-                if(j == _.data.currentPage) {
-                    sbTemp += "<li class='selected'>["+j+"]</li>";
-                } else {
-                    sbTemp += "<li onclick='PagingHelper.gotoPage("+j+");'>["+j+"]</li>";        
-                }
-            }            
-
-            // 이전페이지 버튼
-            sb = "<ul>"
-            if(_.data.currentPage > s_page || _.data.totalCnt > _.data.maxListCount && s_page > 1){
-                sb += "<li class='first' onclick='PagingHelper.gotoPage(1);'>처음</li >"
-                sb += "<li class='previous' onclick='PagingHelper.gotoPage(" + (_.data.currentPage - 1) + ");'>이전</li>"    
-            }
-            
-            // 현재블럭의 페이지 목록
-            sb += sbTemp
-            
-            // 다음페이지 버튼
-            if(_.data.currentPage < _.data.totalPageCnt ){
-                sb += "<li class='next' onclick='PagingHelper.gotoPage(" + (_.data.currentPage + 1) + ");'>다음</li>"
-                sb += "<li class='last' onclick='PagingHelper.gotoPage(" + (_.data.totalPageCnt) +");'>마지막</li >"            
-            }
-            sb += "</ul>";
-                        
-            return sb;
-        },
-        "makeNum":function(className, content ){     //필요없음.    
-            return "<li class='"+className+"''>["+content+"]</li>";
-        },
-        'setStartnumEndnum' : function() {
-            // 시작 글번호
-            this.data.startnum = (this.data.currentPage -1) * this.data.maxListCount + 1;
-
-            // 마지막 글번호
-            var tmp = this.data.currentPage * this.data.maxListCount;
-            this.data.lastnum = (tmp > this.data.totalCnt?this.data.totalCnt:tmp);
-        },
-        'gotoPage':function(pageNum){
-            console.log(pageNum);
-                        
-            this.data.currentPage = pageNum; //입력받은 페이지번호를 현재페이지로 설정
-            this.setStartnumEndnum();    //입력받은 페이지의 startnum과 endnum구하기
-
-            //콘솔 출력 (삭제)
-            console.log(this.data.currentPage+"/"+this.data.startnum +"/"+this.data.lastnum);
-            //alert(this.data.currentPage+"/"+this.data.startnum +"/"+this.data.lastnum);
-            
-            //리스트 불러오는 ajax 호출
-            //////////////////////////
-            $("#paging").html(this.pagingHtml()); 
-        }
-    }
-
-/////////////////////////////////////////////////////////////////////
-//테스트용 : 임시로 총레코드수 설정.
-////////////////////////////////////////////////////////////////////
-
-//PagingHelper.data.totalCnt = 1001;
-
-
-	
-	
--- Balpoom/src/controller/main_controller.java
+++ Balpoom/src/controller/main_controller.java
@@ -1,20 +1,37 @@
 package controller;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import models.roomDao;
+import utils.Urlpicture;
+import utils.Urlpictures;
 
 @Controller
 public class main_controller {
 	
 	@Autowired
 	roomDao rd;
+	@Autowired
+	Urlpicture urp;
+	@Autowired
+	Urlpictures urps;
 	
 	@RequestMapping("/")
 	public ModelAndView mainHandler() {
@@ -23,16 +40,89 @@ public class main_controller {
 	}
 	
 	@RequestMapping("/search")
-	public ModelAndView searchHandler() {
+	public ModelAndView searchHandler() throws JsonProcessingException {
 		ModelAndView mav = new ModelAndView("t_search");
-		return mav;
-	}
+		List<HashMap<String, Object>> mlist = rd.test_room(); // 데이터 베이스 처리 결과 input
+// ===================================================== /search 처리 코드 
+		List<Integer> numbers =new ArrayList<>();
+		int defnum = 7682481;
+		List<String> urlcol = new ArrayList<>();
+		JSONArray urlcolj= null;
+		JSONArray numbersj = null;
 	
-	@RequestMapping("/detail")
-	public ModelAndView detailViewHandler() {
-		ModelAndView mav = new ModelAndView("t_detail");
+
+		JSONArray json_arr = new JSONArray();// Json 객체 로 만들기
+		for (Map<String, Object> map : mlist) {
+			JSONObject json_obj = new JSONObject();
+			String url = null;
+			String input = null;
+			//System.out.println(map.get("SELL_NUM"));
+			int num = 7666495;
+			num = Integer.parseInt(map.get("SELL_NUM") + "");
+			input = "https://www.zigbang.com/items1/" + num;
+		
+			try {
+				//System.out.println("urp=" + urp.get_main_url(input));
+				url = (urp.get_main_url(input));
+				numbers.add(num);} catch (Exception e) {
+			
+				//e.printStackTrace();
+				url = "http://z1.zigbang.com/items/"+defnum +"/538ec7a16033f0c32eac29ac50deb04a1e6bf3ba.jpg?h=800&q=60";
+			}
+			//System.out.println(url);
+			urlcol.add(url);
+			urlcolj = new JSONArray(urlcol);
+			numbersj = new JSONArray(numbers);
+			String temp=new ObjectMapper().writeValueAsString(urlcol);
+			//System.out.println(temp);
+			//System.out.println("urlcolj="+urlcolj.toString());
+			for (Map.Entry<String, Object> entry : map.entrySet()) {
+				String key = entry.getKey();
+				Object value = entry.getValue();
+
+				try {
+
+					json_obj.put(key, value);
+				} catch (Exception e) {
+					// TODO Auto-gen erated catch block
+					//e.printStackTrace();
+				}
+			}
+			(json_arr).put(json_obj);
+
+		}
+
+	
+		//System.out.println("numbers= "+ numbersj.toString());
+		mav.addObject("dn", defnum );
+		mav.addObject("nj",numbersj);
+		mav.addObject("mpic", urlcolj);
+		mav.addObject("mlist", json_arr.toString());
+		mav.addObject("msize", mlist.size());
+		mav.setViewName("t_search");
+//=========================================================================search code처리 
 		return mav;
+
 	}
+
+	@RequestMapping("/detail")
+	public ModelAndView detailViewHandler(@RequestParam Map n, HttpServletRequest req) {
+		//사진처리 
+		JSONArray picturesJ= null;
+		Urlpictures ulps = new Urlpictures();
+		int num = Integer.parseInt(n.get("num")+"");
+	
+		
+		System.out.println("num======================>"+num);
+		List<String> all=  ulps.get_picture_urls("https://www.zigbang.com/items1/"+num);
+		picturesJ = new JSONArray(all);
+		ModelAndView mav = new ModelAndView("t_detail");
+		mav.addObject("pj", all);	
+	
+		return mav;
+
+	}
+
 	//test 용 신경쓰지 말것
 	@RequestMapping("/testing")
 	public ModelAndView testing(){
