5ab850313d99009ddd0a75d619ca4df2aeb6da17
admin <admin@192.168.10.5>
2017-03-21 19:04:31 +0900

    지도가 뜬다!!!

--- Balpoom/src/controller/roomadd_controller.java
+++ Balpoom/src/controller/roomadd_controller.java
@@ -1,11 +1,19 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping("/")
@@ -19,10 +27,26 @@ public class roomadd_controller {
 	}
 	
 	@RequestMapping("/roomMap")
-	public ModelAndView mav(@RequestParam Map map){
+	@ResponseBody
+	public Map mav(@RequestParam (name="map") String addMap) throws IOException{
 		ModelAndView mav = new ModelAndView();
-		
-		return mav;
+		addMap = addMap.replaceAll("\\s","");
+		String target = "https://maps.googleapis.com/maps/api/geocode/json?address=" + addMap
+				+ "&key=AIzaSyBS-83LLE8F1nvtVtsy1Adu-j4LeS9qAQg&language=ko";
+		URL url = new URL(target);
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+		String outstr = "";
+		while (true) {
+			String str = br.readLine();
+			if (str == null)
+				break;
+			// out.println(str);
+			outstr += str;
+		}
+
+		// out.println(outstr);
+		Map map = new ObjectMapper().readValue(outstr, Map.class);
+		return map;
 		
 	}
 	
