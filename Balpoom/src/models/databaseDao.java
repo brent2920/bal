e90bb4a914361ac0b8a29136b7a93632a955c776
admin <admin@192.168.10.6>
2017-03-22 13:37:02 +0900

    푸시푸시

--- Balpoom/src/models/databaseDao.java
+++ Balpoom/src/models/databaseDao.java
@@ -29,7 +29,7 @@ public class databaseDao {
 		try {
 
 
-			BufferedReader brr = new BufferedReader(new FileReader("D:\\numbers.txt"));
+			BufferedReader brr = new BufferedReader(new FileReader("D:\\3.21.txt"));
 
 			while (true) {
 			
@@ -56,36 +56,44 @@ public class databaseDao {
 				}
 				br.close();
 
-				JSONParser jp = new JSONParser();
-				JSONObject jo = (JSONObject) jp.parse(result);
-				JSONArray ja = (JSONArray) jo.get("items");
-				JSONObject joo = (JSONObject) ja.get(0);
-				JSONObject jo1 = (JSONObject) joo.get("item");
+				JSONParser jp = new JSONParser(); //JSON-SIMPLE이라는 라이브러리에서 JSON을 파싱하기 위해 사용된 객체
+				JSONObject jo = (JSONObject) jp.parse(result); //String을 JSONObject로 캐스팅해서 JSON형태로 변경
+				JSONArray ja = (JSONArray) jo.get("items"); //JSON 형태에서 배열[]로 표시된 배열명을 추출
+				JSONObject joo = (JSONObject) ja.get(0);//위에 배열명의 0번째 index를 가져옴
+				JSONObject jo1 = (JSONObject) joo.get("item");//0번째 배열의 item이라는 컬럼명을 가져옴
 
-				int sell_num = Integer.parseInt(jo1.get("id").toString());
+				int sell_num = Integer.parseInt(jo1.get("id").toString()); //item 안에 있는 id의 value를 가져옴 아래로 같은 형태로 진행
+				//id ===> room 테이블에서 sell_num으로 사용
 				String b_title = joo.get("title").toString();
+				//title ===> room 테이블에서 b_title로 사용
 				int b_deposit = Integer.parseInt(jo1.get("deposit").toString());
+				//deposit ===> room 테이블에서 b_deposit로 사용
 				int b_mpay = Integer.parseInt(jo1.get("rent").toString());
+				//rent ===> room 테이블에서 b_mpay로 사용
 				String b_rkind = jo1.get("room_type").toString();
+				//room_type ===> room 테이블에서 b_rkind로 사용
 				String floor = jo1.get("floor").toString();
+				
 				String[] ar = new String[]{}; 
-				int b_floor = 0;
+				String b_floor ="";
 				
 				if(!(floor.isEmpty())){
-					System.out.println(floor.toString());
+					
 					if(floor.contains("/")){
 						ar = floor.split("/");
-						b_floor = Integer.parseInt(ar[1].toString().substring(0,ar[1].indexOf("층")));
+						b_floor = ar[1].toString();
 					}else{
-						b_floor = Integer.parseInt(floor.toString().substring(0,floor.indexOf("층")));
+						b_floor = floor;
 					}
 				}
 
-				String floor_all = jo1.get("floor_all").toString();
-				int b_floor_all = Integer.parseInt(floor_all.substring(0, floor_all.lastIndexOf("층")));
+				String b_floor_all = jo1.get("floor_all").toString();
+				//floor_all ===> room 테이블에서 b_floor_all로 사용
 				
 				double b_size_m2 = Double.parseDouble(jo1.get("size_m2").toString());
+				//size_m2 ===> room 테이블에서 b_size_m2로 사용
 				double b_size = Double.parseDouble( jo1.get("size").toString());
+				//size ===> room 테이블에서 b_size로 사용
 				
 				String b_gpay = jo1.get("manage_cost").toString();
 				String b_glist = jo1.get("manage_cost_inc").toString();
@@ -164,7 +172,7 @@ public class databaseDao {
 
 			Map<String, Object> map = new HashMap<>();
 
-			BufferedReader brr = new BufferedReader(new FileReader("d:\\numbers.txt"));
+			BufferedReader brr = new BufferedReader(new FileReader("d:\\3.21.txt"));
 
 			while (true) {
 
@@ -190,21 +198,32 @@ public class databaseDao {
 				}
 				br.close();
 
-				JSONParser jp = new JSONParser();
-				JSONObject jo = (JSONObject) jp.parse(result);
-				JSONArray ja = (JSONArray) jo.get("items");
-				JSONObject joo = (JSONObject) ja.get(0);
-				JSONObject jo1 = (JSONObject) joo.get("item");
+				JSONParser jp = new JSONParser(); //JSON-simple 라이브러리에서 JSONPaser라는 객체를 소환
+				JSONObject jo = (JSONObject) jp.parse(result);//String 값을 JSONObject로 캐스팅
+				JSONArray ja = (JSONArray) jo.get("items"); //items라는 배열로 된 JSON를 JSONArray로 캐스팅해서 index로 값을 뽑기 위해 준비
+				JSONObject joo = (JSONObject) ja.get(0); //0번째 index를 추출해서 JSONObject로 캐스팅
+				JSONObject jo1 = (JSONObject) joo.get("item"); // 0번째 index로 추출한 json형태에서 item을 추출
+				
+				
 
 				String bk_officename = jo1.get("agent_name").toString();
+				//agent_name ==> agent테이블명에서 bk_officename로 사용
 				String bk_num = jo1.get("bjd_code").toString();
+				//bjd_code ==> agent테이블명에서 bk_num로 사용
 				String bk_address = jo1.get("agent_address1").toString();
+				//agent_address1 ==> agent테이블명에서 bk_address로 사용
 				String bk_agentnum = jo1.get("agent_phone").toString();
+				//agent_phone ==> agent테이블명에서 bk_agentnum로 사용
 				String bk_regname = jo1.get("user_name").toString();
+				//user_name ==> agent테이블명에서 bk_regname로 사용
 				String bk_contact = jo1.get("original_user_phone").toString();
+				//original_user_phone ==> agent테이블명에서 bk_contact로 사용
 				String bk_email = jo1.get("agent_email").toString();
+				//agent_email ==> agent테이블명에서 bk_email로 사용
 				String bk_password = jo1.get("user_no").toString();
+				//user_no ==> agent테이블명에서 bk_password로 사용
 				String id = jo1.get("user_email").toString();
+				//user_email ==> agent테이블명에서 id로 사용
 				
 				
 				int sell_num = Integer.parseInt(jo1.get("id").toString());
