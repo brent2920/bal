009a9e96e06d122f0d76f5a0471e028447f30726
Brent Jung <Brent Jung@DESKTOP-16IB7IT>
2017-03-24 18:47:33 +0900

    커밋

--- Balpoom/src/controller/detail_controller.java
+++ Balpoom/src/controller/detail_controller.java
@@ -66,7 +66,7 @@ public class detail_controller {
 			String BK_REGNAME =	map.get("BK_REGNAME").toString().substring( 
 					agentmap.get("BK_REGNAME").toString().indexOf("("),
 					agentmap.get("BK_REGNAME").toString().indexOf(")"));
-			agentmap.put("BK_REGNAME", BK_REGNAME);
+				agentmap.put("BK_REGNAME", BK_REGNAME);
 			}
 			
 			
