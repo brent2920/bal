65af06d562e0f55b54c324c794a1958199550767
Brent Jung <Brent Jung@DESKTOP-16IB7IT>
2017-03-29 15:57:44 +0900

    api 키 변경

--- Balpoom/src/utils/APIKeys.java
+++ Balpoom/src/utils/APIKeys.java
@@ -7,7 +7,7 @@ public class APIKeys {
 	//AIzaSyB7rPQITd2SVLFx6SiEhk8P_GPWxQN32Y0
 	//AIzaSyAAE5JejbaQYPmos43wLK3mBPVlkonWUOE
 	//AIzaSyAkcL0U9iE0uwxqFMSvh7nhM6cFRmyynzk
-	static String GOOGLE_MAP_KEY = "AIzaSyD4vzBQiYomL-S8Fwvp1649W56W_9SmDE4";
+	static String GOOGLE_MAP_KEY = "AIzaSyA2FVNxtg1EXOHZ4CjQoV8VTCkOTT6VD0M";
 
 	public static String getGOOGLE_MAP_KEY() {
 		return GOOGLE_MAP_KEY;
