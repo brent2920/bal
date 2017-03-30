16e89c9d16dfa1a8ea3ba90c873a998313b3d3f5
admin <admin@192.168.10.5>
2017-03-30 15:03:19 +0900

    뉴스

--- Balpoom/src/controller/logout_controller.java
+++ Balpoom/src/controller/logout_controller.java
@@ -1,17 +1,29 @@
 package controller;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import models.newsDao;
+
 @Controller
 @RequestMapping("/")
 public class logout_controller {
+	@Autowired
+	newsDao ndao;
+	
+	
 		@RequestMapping("/logout")
 		public ModelAndView logout(HttpSession session){
 			ModelAndView mav = new ModelAndView();
+			List<Map> news = ndao.get_news();
+			mav.addObject("news",news);
 			
 			session.removeAttribute("id");
 			session.removeAttribute("id1");
