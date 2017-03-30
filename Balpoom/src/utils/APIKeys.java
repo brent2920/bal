15272eef9604b7d5eeae14d159bca6db856ea438
admin <admin@192.168.10.5>
2017-03-30 15:26:03 +0900

    지도 키

--- Balpoom/src/utils/APIKeys.java
+++ Balpoom/src/utils/APIKeys.java
@@ -9,7 +9,7 @@ public class APIKeys {
 	//AIzaSyAkcL0U9iE0uwxqFMSvh7nhM6cFRmyynzk
 	//AIzaSyD4vzBQiYomL-S8Fwvp1649W56W_9SmDE4
 
-	static String GOOGLE_MAP_KEY = "AIzaSyB7rPQITd2SVLFx6SiEhk8P_GPWxQN32Y0";
+	static String GOOGLE_MAP_KEY = "AIzaSyCm6aXkRRr_msMRm2QN22uiaL5SHvlqMJY";
 
 	public static String getGOOGLE_MAP_KEY() {
 		return GOOGLE_MAP_KEY;
